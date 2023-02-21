e70545d81ceb32800ce9a6c6f2eb2df6575dbc46
Sandeep Sharma <sandeeptech8@gmail.com>
2023-02-21 17:14:09 +0530

    add <Sidebar />

--- electron-vite-react/src/App/organisms/db/dashboard/DbDashboard.jsx
+++ electron-vite-react/src/App/organisms/db/dashboard/DbDashboard.jsx
@@ -1,19 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import './DbDashboard.scss';
+
+import Button from '../../../atoms/button/Button';
 import SqlServerInfoForm from "./SqlServerInfoForm";
 
-function DbDashboard({}) {
 
+
+
+const sidebarItems = [{
+  text: 'Sql Server Settings',
+  type: 'sql-server-details',
+}, {
+  text: 'Monitor',
+  type: 'monitor',
+}];
+
+function Sidebar({ onSelect }) {
+  return (
+    <div className="sidebar">
+      {sidebarItems.map((item) => (
+        <Button
+          onClick={() => onSelect(item)}
+          key={item.type}
+        >
+          {item.text}
+        </Button>
+      ))}
+    </div>
+  );
+}
+
+function Monitor() {
+  return (
+    <div>
+      monitor
+    </div>
+  );
+}
+
+function DbDashboard({}) {
+  const [content, setContent] = useState(<div />);
 
   const onServerDetailsFormSubmit = async (details) => {
-    const res = await window.mainAPI.connectToDb(details);
-    console.log('DEBUG :: res in renderer ::', res);
+    const res = await window.mainAPI.setupConfig(details);
+  };
+
+  const onSidebarSelect = (sidebarItem) => {
+    switch (sidebarItem.type) {
+      case 'sql-server-details': {
+        setContent(<SqlServerInfoForm onSubmit={onServerDetailsFormSubmit} />);
+        break;
+      }
+      case 'monitor': {
+        setContent(<Monitor />);
+        break;
+      }
+      default: setContent(<div />);
+    }
   }
 
 
   return (
-    <div>
-      <p>Database dashboard</p>
-      <SqlServerInfoForm onSubmit={onServerDetailsFormSubmit} />
+    <div className="dashboard">
+      <h1 className="dashboard__title">Database Dashboard</h1>
+      <div className='dashboard__content'>
+        <Sidebar onSelect={onSidebarSelect} />
+        <div className="sidebar-content">
+          {content}
+        </div>
+      </div>
     </div>
   )
 }
