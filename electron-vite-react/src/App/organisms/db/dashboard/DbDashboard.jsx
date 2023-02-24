48c2bdc5315ca95e3e7dc402b638477febd3394d
Sandeep Sharma <sandeeptech8@gmail.com>
2023-02-24 17:10:55 +0530

    render data in the table

--- electron-vite-react/src/App/organisms/db/dashboard/DbDashboard.jsx
+++ electron-vite-react/src/App/organisms/db/dashboard/DbDashboard.jsx
@@ -45,17 +45,41 @@ function Sidebar({ onSelect }) {
 
 function Table({ data, columns }) {
   const table = useReactTable({
-    data: [],
-    columns: [],
+    data: data,
+    columns: columns,
     getCoreRowModel: getCoreRowModel(),
   })
 
-  console.log(table.getHeaderGroups());
-  console.log(table.getAllColumns());
   return (
-    <div className="monitor__content__table">
-      table placeholder
-    </div>
+    <table className="monitor__content__table">
+      <thead>
+        {table.getHeaderGroups().map(headerGroup => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map(header => (
+              <th key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map(row => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map(cell => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
